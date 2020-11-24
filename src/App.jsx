72d8e6a4620ddffd374871c8639d9e8b6b608ebd
deepnothing <65370281+deepnothing@users.noreply.github.com>
2020-11-24 16:48:43 -0500

    add: initialCommit

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ import Cart from './Cart/Cart'
 import CompletedOrders from './CompletedOrders/CompletedOrders'
 import DeliveredOrders from './DeliveredOrders/DeliveredOrders'
 import OpenOrders from './Open Orders/OpenOrders'
-import NavBar from './GlobalComponents/NavBar' 
+import NavBar from './GlobalComponents/NavBar'
 
 const App = () => (
 	<Router>
 		<div>
 			<NavBar />
+			<Home />
 			<Switch>
-				<Route path='/' exact component={ Home }/>
-				<Route path='/CreateNewOrder' component={ CreateNewOrder }/>
-				<Route path='/Cart' component={ Cart }/>
-				<Route path='/OpenOrders' component={ OpenOrders }/>
-				<Route path='/DeliveredOrders' component={ DeliveredOrders }/>
-				<Route path='/CompletedOrders' component={ CompletedOrders }/>
+				<Route path='/CreateNewOrder' component={CreateNewOrder} />
+				<Route path='/Cart' component={Cart} />
+				<Route path='/OpenOrders' component={OpenOrders} />
+				<Route path='/DeliveredOrders' component={DeliveredOrders} />
+				<Route path='/CompletedOrders' component={CompletedOrders} />
 			</Switch>
 		</div>
 	</Router>
